76e691abb6ac10bb284d2f006c98b70310436aa6
araalifarooq <farooqp35h@gmail.com>
2018-08-17 21:50:54 +0300

    Add a post question method

---
+++ app/views.py
@@ -0,0 +1,18 @@
+from app import app
+from flask import request, jsonify, json
+from app.models import Answer, Question
+
+all_questions = []
+all_answers = []
+
+
+@app.route("/api/v1/questions", methods=["POST"])
+# posting a single question
+def post_question():
+    data = request.get_json()
+    question = data.get("question")
+    qstn_id = len(all_questions) + 1
+
+    new_question = Question(qstn_id , question)
+    all_questions.append(new_question)
+    return jsonify({"message":"New question successfully posted"}), 201
